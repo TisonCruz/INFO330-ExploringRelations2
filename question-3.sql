6fb7a210041a86401f02a39b2cfc06ee57329ecb
TisonCruz <122429561+TisonCruz@users.noreply.github.com>
2023-04-11 21:46:23 -0700

    Update question-3.sql

--- question-3.sql
+++ question-3.sql
@@ -1,1 +1,8 @@
 -- Provide a query that includes the purchased track name AND artist name with each invoice line item.
+
+select tracks.name as 'Track Name', artists.name as 'Artist Name', invoice_items.InvoiceLineId,
+invoice_items.InvoiceId, invoice_items.TrackId, invoice_items.UnitPrice, invoice_items.Quantity
+from tracks, albums, artists, invoice_items
+where invoice_items.TrackId = tracks.TrackId
+and albums.AlbumId = tracks.AlbumId
+and albums.ArtistId = artists.ArtistId;

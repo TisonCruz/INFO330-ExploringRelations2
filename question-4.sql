488fdb438cb2f5581491bb3d3df3205653673471
TisonCruz <122429561+TisonCruz@users.noreply.github.com>
2023-04-11 22:41:10 -0700

    Update question-4.sql

--- question-4.sql
+++ question-4.sql
@@ -1,1 +1,10 @@
 -- Which sales agent made the most in sales in 2010?
+
+select employees.FirstName, employees.LastName, Invoices.InvoiceDate, max(invoices.Total) as 'Total Sales'
+from employees, customers, invoices
+where employees.EmployeeId = customers.SupportRepId
+and invoices.CustomerId = customers.CustomerId
+and employees.Title = 'Sales Support Agent'
+and invoices.InvoiceDate like '2010%'
+order by max(invoices.total)
+limit 1;

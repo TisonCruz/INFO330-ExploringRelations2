ca5e8d13d529f9e22e1f44025e52bd5181e5d1e6
TisonCruz <122429561+TisonCruz@users.noreply.github.com>
2023-04-11 22:55:32 -0700

    Update question-5.sql

--- question-5.sql
+++ question-5.sql
@@ -1,1 +1,9 @@
 -- What was the most purchased track of 2013?
+
+select tracks.name as 'Most Purchased Track', max(tracks.TrackId) as 'Number of Tracks Bought', invoices.InvoiceDate as 'Year'
+from tracks, invoices, invoice_items
+where tracks.TrackId = invoice_items.TrackId
+and invoices.InvoiceId = invoice_items.InvoiceId
+and invoices.InvoiceDate like '2013%'
+order by max(tracks.TrackID)
+limit 1;
